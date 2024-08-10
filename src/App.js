7fc56be0a90f6955c1f7a560a3e9a619612ec788
Rohit.pandey <pandey48400@gmail.com>
2024-08-10 20:07:12 +0530

    Updated files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+const github = new Github();
+
+const ui = new UI();
+
+const searchUser = document.getElementById("searchUser");
+
+// Add event listener to search input
+searchUser.addEventListener("keyup", (e) => {
+  // Get the text that is being typed in
+  const userText = e.target.value;
+  // console.log(userText);
+
+  // Make sure input is not blank
+  if (userText !== "") {
+    // Continue making http call to GitHub API
+
+    // Make HTTP call to get user
+    github.getUser(userText).then((data) => {
+      console.log(data);
+      if (data.profile.message === "Not Found") {
+        // Show Alert that user is not Found
+        // This will happen through ui class (ui.js)
+        ui.showAlert("User not found!", "alert alert-danger");
+      } else {
+        // Show Profile
+        // This will happen through ui class (ui.js)
+        ui.showProfile(data.profile);
+
+        // Show repositories
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    // Clear the profile
+    // This will happen through ui class (ui.js)
+    ui.clearProfile();
+  }
+});
